26b076422fa45928ccb82ebbb1f6ac82806e10cc
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2013-01-23 19:50:55 -0500

    trying out high voltage

--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,7 @@ IDCD::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'high_voltage/pages#show', :id => 'about'
 
   # See how all your routes lay out with "rake routes"
 
