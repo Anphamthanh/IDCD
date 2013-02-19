b4677c24b8e524162115e0a81c8b65405ffd7d5b
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2013-02-19 12:06:26 -0500

    Added two helper methods user_present and type_of_user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  def user_present
+    if current_student or current_faculty or current_admin
+      return true
+    else
+      return false
+    end
+  end
+
+  def type_of_user
+    if current_student
+      return "student"
+    elsif current_admin
+      return "admin"
+    elsif current_faculty
+      return "faculty"
+    else
+      return "no_user"
+    end
+  end
+
 end
