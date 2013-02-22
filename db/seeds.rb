7a11567be6c9cbd49ec2c8fc9539095edebfd567
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2013-02-21 22:23:07 -0500

    tweaked page text views. added page seeds for home, expo and industry

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,14 @@
 Semester.create(:name => "Spring 2013", :start => Time.local(2013, 1, 10), :end => Time.local(2013, 5, 2))
 Company.create(:name => "Boeing", :technical_name => "Survy")
 Project.create(:name => "Jet Packs", :background => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum sodales lacus, vitae dictum massa cursus vitae. Fusce porttitor nulla non velit porta mattis.", status_id: 1, :company_id => 1)
-PastProject.create(:title => "NASA Space Trains", :description => "Coming soon");
+PastProject.create(:title => "NASA Space Trains", :description => "Coming soon")
 
-ProjectStatus.create(:name => "Incomplete");
-ProjectStatus.create(:name => "Submitted");
-ProjectStatus.create(:name => "Accepted");
-ProjectStatus.create(:name => "Rejected");
+ProjectStatus.create(:name => "Incomplete")
+ProjectStatus.create(:name => "Submitted")
+ProjectStatus.create(:name => "Accepted")
+ProjectStatus.create(:name => "Rejected")
+
+Page.create(:name => "Home")
+Page.create(:name => "Expo")
+Page.create(:name => "Industry")
+
