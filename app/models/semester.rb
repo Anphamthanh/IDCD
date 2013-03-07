1e94b3f112a79255b75541e0efb281e4d1d62132
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2013-03-06 21:16:58 -0500

    temporary commit (while I go to kurts)

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -1,3 +1,4 @@
 class Semester < ActiveRecord::Base
   attr_accessible :end, :name, :start
+  has_many :sections
 end
--- test/unit/helpers/project_setup_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ProjectSetupHelperTest < ActionView::TestCase
-end
