57de4e1031adfc209df2b14f1a65aaa572da90d0
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2013-04-03 20:37:30 -0400

    fixed paths for pictures.Also hid page edit controls for admins only

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,6 +2,7 @@ class PicturesController < ApplicationController
   # GET /pictures
   # GET /pictures.json
   def index
+    @page = Page.find(params[:page_id])
     @pictures = Picture.all
 
     respond_to do |format|
@@ -13,6 +14,7 @@ class PicturesController < ApplicationController
   # GET /pictures/1
   # GET /pictures/1.json
   def show
+    @page = Page.find(params[:page_id])
     @picture = Picture.find(params[:id])
 
     respond_to do |format|
@@ -24,6 +26,7 @@ class PicturesController < ApplicationController
   # GET /pictures/new
   # GET /pictures/new.json
   def new
+    @page = Page.find(params[:page_id])
     @picture = Picture.new
 
     respond_to do |format|
@@ -34,17 +37,19 @@ class PicturesController < ApplicationController
 
   # GET /pictures/1/edit
   def edit
+    @page = Page.find(params[:page_id])
     @picture = Picture.find(params[:id])
   end
 
   # POST /pictures
   # POST /pictures.json
   def create
+    @page = Page.find(params[:page_id])
     @picture = Picture.new(params[:picture])
 
     respond_to do |format|
       if @picture.save
-        format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
+        format.html { redirect_to page_picture_path(@page,@picture), notice: 'Picture was successfully created.' }
         format.json { render json: @picture, status: :created, location: @picture }
       else
         format.html { render action: "new" }
@@ -56,11 +61,12 @@ class PicturesController < ApplicationController
   # PUT /pictures/1
   # PUT /pictures/1.json
   def update
+    @page = Page.find(params[:page_id])
     @picture = Picture.find(params[:id])
 
     respond_to do |format|
       if @picture.update_attributes(params[:picture])
-        format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
+        format.html { redirect_to page_picture_path(@page,@picture), notice: 'Picture was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -72,11 +78,12 @@ class PicturesController < ApplicationController
   # DELETE /pictures/1
   # DELETE /pictures/1.json
   def destroy
+    @page = Page.find(params[:page_id])
     @picture = Picture.find(params[:id])
     @picture.destroy
 
     respond_to do |format|
-      format.html { redirect_to pictures_url }
+      format.html { redirect_to page_pictures_path(@page) }
       format.json { head :no_content }
     end
   end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,5 @@
 class Picture < ActiveRecord::Base
+  mount_uploader :name, PictureUploader
   attr_accessible :name
+  belongs_to :page
 end
