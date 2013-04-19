75d2ed43539f7af910245ff7e77d18c1c0ca52fa
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2013-04-19 09:56:57 -0400

    added projects, companies and groups seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,10 +24,28 @@ School.create(:name => "ME")
 School.create(:name => "BME")
 
 Company.create(:name => "Boeing", :technical_name => "Survy")
+Company.create(:name => "Georgia Power", :technical_name => "John Smith")
+Company.create(:name => "National Instruments", :technical_name => "Mark Jones")
+Company.create(:name => "Cooper Lighting", :technical_name => "George Johnson")
+Company.create(:name => "Delta Airlines Instruments", :technical_name => "Stan Brown")
+
 Project.create(:name => "Jet Packs", :background => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum sodales lacus, vitae dictum massa cursus vitae. Fusce porttitor nulla non velit porta mattis.", :company_id => 1)
+Project.create(:name => "Automatic Pulverizer Pyrite Flushing System for Units 1-3", :background => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum sodales lacus, vitae dictum massa cursus vitae. Fusce porttitor nulla non velit porta mattis.", :company_id => 2)
+Project.create(:name => "Light Emitting Gate Analysis Board", :background => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum sodales lacus, vitae dictum massa cursus vitae. Fusce porttitor nulla non velit porta mattis.", :company_id => 3)
+Project.create(:name => "LED Hose Down fixture", :background => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum sodales lacus, vitae dictum massa cursus vitae. Fusce porttitor nulla non velit porta mattis.", :company_id => 4)
+Project.create(:name => "Prevention of aircraft damage during loading", :background => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum sodales lacus, vitae dictum massa cursus vitae. Fusce porttitor nulla non velit porta mattis.", :company_id => 5)
+
 ProjectStatus.create(:project_id => 1, :project_status_type_id => 1)
+ProjectStatus.create(:project_id => 2, :project_status_type_id => 1)
+ProjectStatus.create(:project_id => 3, :project_status_type_id => 1)
+ProjectStatus.create(:project_id => 4, :project_status_type_id => 1)
+ProjectStatus.create(:project_id => 5, :project_status_type_id => 1)
+
 PastProject.create(:title => "NASA Space Trains", :description => "Coming soon")
 
+Group.create(:name => "Group 1")
+Group.create(:name => "Group 2")
+Group.create(:name => "Group 3")
 
 # Stuff that will not change without a significant code impact
 # 
