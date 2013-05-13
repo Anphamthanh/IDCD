4f9238ada2fde9aa46e0bf4dfa1cb67031f16188
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2013-05-12 19:12:33 -0700

    prepopulate and save new user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,16 @@ class HomeController < ApplicationController
   before_filter RubyCAS::Filter, :only => [:login_with_CAS]
 
   def login_with_CAS
-#    if Member.find_by_gtusername(session[:cas_user]).nil?
-#      flash[:notice] = "Welcome new user! Please fill out these details to get started"
-#      redirect_to :controller => 'members', :action => 'new', :newUser => 'true', :gtusername => session[:cas_user]
-#      return
-#    end
+    if User.find_by_gtusername(session[:cas_user]).nil?
+      flash[:notice] = "Welcome new user! Please fill out these details to get started"
+      
+      # create and save a new user
+      # pre-populate details received from CAS
+      @user = User.create(:gtusername => session[:cas_user])
+
+      redirect_to edit_user_path(@user)
+      return
+    end
 
     respond_to do |format|
       format.html { redirect_to :controller => 'pages', :action => 'index' }
