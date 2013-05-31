d9a8c7b3858c0eebe1da8303268b9f346eeabaa4
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2013-05-31 01:28:43 -0700

    semester settings. changed way of finding approved projects for faculty

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -4,9 +4,31 @@ class Faculty < User
   has_many :sections, :through => :faculty_sections
   has_many :faculty_project_decisions
   has_many :projects, :through => :faculty_project_decisions
-  has_many :approved_projects, :through => :faculty_project_decisions, :source => :project, :conditions => "decision = true"
-  has_many :rejected_projects, :through => :faculty_project_decisions, :source => :project, :conditions => "decision = false"
+  #has_many :approved_projects, :through => :faculty_project_decisions, :source => :project, :conditions => "decision = true"
+  #has_many :rejected_projects, :through => :faculty_project_decisions, :source => :project, :conditions => "decision = false"
   
   belongs_to :school
+
+  def approved_projects
+    return_list = []
+    projects = self.projects.where("decision = true and project_status_id = 3")
+    projects.each do |project|
+      if project.schools.include? self.school
+        return_list << project
+      end
+    end
+    return return_list
+  end
+
+  def rejected_projects
+    return_list = []
+    projects = self.projects.where("decision = false and project_status_id = 3")
+    projects.each do |project|
+      if project.schools.include? self.school
+        return_list << project
+      end
+    end
+    return return_list
+  end
 
 end
