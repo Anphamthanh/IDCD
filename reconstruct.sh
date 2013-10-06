acb8b4be2a6ccdde47b48d1b3b5f3c9ea6470c22
passenger <passenger@medesign.me.gatech.edu>
2013-10-06 17:28:11 -0400

    saving server files

--- reconstruct.sh
+++ reconstruct.sh
@@ -1,4 +1,5 @@
-rake db:drop
-rake db:create
-rake db:migrate
-rake db:seed
+git pull
+sudo service httpd stop
+RAILS_ENV=production rake db:migrate
+RAILS_ENV=production rake assets:precompile
+sudo service httpd start
